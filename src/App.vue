15e50ae2df9337f70f9398b2c0e694ed985a7601
spitchell <mr.spitchell@gmail.com>
2020-09-16 14:37:21 +0100

    basic card component working

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,22 @@
 <template>
   <div id="app">
     <div v-for="country in countries" :key="country.alpha2code">
-      <p>{{ country.name }}</p>
+      <CountryCard
+        :name="country.name"
+        :flag="country.flag"
+        :population="country.population"
+        :region="country.region"
+        :capital="country.capital"
+      />
     </div>
   </div>
 </template>
 
 <script>
+import CountryCard from './components/CountryCard';
 export default {
   name: 'App',
+  components: { CountryCard },
   data() {
     return {
       countries: this.$parent.data
@@ -17,7 +25,7 @@ export default {
 };
 </script>
 
-<style>
+<style lang="scss">
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -25,5 +33,9 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+  display: flex;
+  > * + * {
+    margin-left: 1rem;
+  }
 }
 </style>
---
+++ src/components/CountryCard.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="card">
+    <Article>
+      <div class="flag-wrapper">
+        <img :src="flag" :alt="name + ' flag'" />
+      </div>
+      <div class="stat-wrapper">
+        <h2 class="heading">{{ name }}</h2>
+        <p>Population: {{ population }}</p>
+        <p>Region: {{ region }}</p>
+        <p>Capital: {{ capital }}</p>
+      </div>
+    </Article>
+  </div>
+</template>
+
+<style lang="scss">
+.card {
+  width: 264px;
+  border-radius: 5px;
+  overflow: hidden;
+  background-color: white;
+  box-shadow: 0px 0px 7px 2px rgba(0, 0, 0, 0.0294384);
+  .heading {
+    text-align: left;
+    margin-top: 0;
+  }
+  .flag-wrapper {
+    height: 11.5rem;
+    img {
+      width: 100%;
+      height: 100%;
+      object-fit: cover;
+    }
+  }
+  .stat-wrapper {
+    padding: 1.5rem;
+    p {
+      text-align: left;
+    }
+  }
+}
+</style>
+
+<script>
+export default {
+  props: {
+    name: String,
+    flag: String,
+    population: Number,
+    region: String,
+    capital: String
+  }
+};
+</script>
